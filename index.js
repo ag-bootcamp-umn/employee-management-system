47ed717a5e410b2e3b3cfcd5dadd0ea8cd0f5fa8
alejandro <alexgeorgegeer@gmail.com>
2024-02-18 01:46:43 -0600

    added cb to functions to restart

--- index.js
+++ index.js
@@ -1,49 +1,42 @@
-// const db = require("./config/connect");
-
 const Prompter = require("./lib/Prompter");
 const Queries = require("./lib/Queries");
 
 const prompter = new Prompter();
 const queries = new Queries();
 
-// async function viewTable(input) {
-//   try {
-//     const data = await queries.selectAll(input);
-//     console.table(data);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
 async function mainMenuSelection() {
   const selection = await prompter.listPrompt(prompter.mainMenuQuery);
   switch (selection) {
     case "View all departments":
-      queries.viewTable("departments");
+      queries.viewTable("departments", followUp);
       break;
 
     case "View all roles":
-      queries.viewTable("roles");
+      queries.viewTable("roles", followUp);
       break;
 
     case "View all employees":
-      queries.viewTable("employees");
+      queries.viewTable("employees", followUp);
       break;
 
     case "Add a department":
-      queries.addDepartment();
+      queries.addDepartment(followUp);
       break;
 
     case "Add a role":
-      queries.addRole();
+      queries.addRole(followUp);
       break;
 
     case "Add an employee":
-      queries.addEmployee();
+      queries.addEmployee(followUp);
       break;
 
     case "Update an employee role":
-      queries.updateEmployeeRole();
+      queries.updateEmployeeRole(followUp);
+      break;
+
+    case "Exit":
+      process.exit();
       break;
 
     default:
@@ -51,5 +44,18 @@ async function mainMenuSelection() {
   }
 }
 
-// mainMenuSelection();
-queries.updateEmployeeRole();
+function followUp() {
+  setTimeout( async () => {
+      const nextStep = await prompter.listPrompt({
+          msg:"What would you like to do next?",
+          opts: ["Another task", "Exit"]
+      });
+      if (nextStep === "Another task") {
+        mainMenuSelection();
+      } else {
+        process.exit();
+      }
+  }, 1000);
+}
+
+mainMenuSelection();
