0349fc3488a2bbf363bb9c8f1e1eb44ac38ecf3b
alejandro <alexgeorgegeer@gmail.com>
2024-02-18 00:01:27 -0600

    simplified functions and abstracted logic

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const db = require("./config/connect");
-const inquirer = require("inquirer");
+// const db = require("./config/connect");
 
 const Prompter = require("./lib/Prompter");
 const Queries = require("./lib/Queries");
@@ -7,114 +6,40 @@ const Queries = require("./lib/Queries");
 const prompter = new Prompter();
 const queries = new Queries();
 
-async function viewTable(input) {
-  try {
-    const data =  await selectAll(input);
-    console.table(data);
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-function selectAll(input) {
-  return new Promise((resolve, reject) => {
-    db.query(`SELECT * FROM ${input};`, (err, data) => {
-      if (err) {
-        console.log(err);
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-}
-
-// function addDepartment() {
-//   prompter.inputPrompt("Enter Department Name").then((data) => {
-//     const sql = `INSERT INTO departments (name) VALUES (?)`;
-//     db.query(sql, data.result, (err, result) => {
-//       console.log("success");
-//     });
-//   });
+// async function viewTable(input) {
+//   try {
+//     const data = await queries.selectAll(input);
+//     console.table(data);
+//   } catch (err) {
+//     console.log(err);
+//   }
 // }
-
-async function addDepartment() {
-  const input = await prompter.inputPrompt('Enter Department Name');
-  db.query(`INSERT INTO departments (name) VALUES (?)`, input, (err, data) => {
-    if (err) console.log(err.message);
-    console.log('Successfully added department.');
-  });
-}
-
-async function addRole() {
-  // Create array for the role and variable for departments
-  const role = {};
-  let departments;
-  // query to get all departments
-  try {
-    const depts = await selectAll('departments');
-    // const deptNames = depts.map(dept => dept.name);
-    departments = depts
-    getDept(departments);
-    return depts;
-  } catch (err) {
-    console.log('Error:', err);
-  }
-  // Have user select a department for the role
-  async function getDept(input) {
-    const dept = await prompter.listPrompt({msg: 'Add the department', opts: input});
-    role.id = departments.find(el => el.name === dept).id;
-    getRoleTitle();
-  }
-  // Get user input for name and salary
-  async function getRoleTitle() {
-    const title = await prompter.inputPrompt('Enter role title');
-    role.title = title;
-    getRoleSalary();
-  }
-  
-  async function getRoleSalary() {
-    const salary = await prompter.numberPrompt('Enter salary');
-    role.salary = salary;
-    inputRole() 
-  }
-
-  // Throw it all into the database
-  async function inputRole() {
-    db.query(`INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?);`, [role.title, role.salary, role.id], (err, data) => {
-      if (err) console.log(err.message);
-      console.log('Successfully added role.');
-    });
-  }
-}
-// addRole();
-
 
 async function mainMenuSelection() {
   const selection = await prompter.listPrompt(prompter.mainMenuQuery);
   switch (selection) {
     case "View all departments":
-      viewTable("departments");
+      queries.viewTable("departments");
       break;
 
     case "View all roles":
-      viewTable("roles");
+      queries.viewTable("roles");
       break;
 
     case "View all employees":
-      viewTable("employees");
+      queries.viewTable("employees");
       break;
 
     case "Add a department":
-      addDepartment();
+      queries.addDepartment();
       break;
 
     case "Add a role":
-      addRole();
+      queries.addRole();
       break;
 
-    case 'Add an employee':
-      console.log("five");
+    case "Add an employee":
+      queries.addEmployee();
       break;
 
     case 6:
@@ -127,3 +52,4 @@ async function mainMenuSelection() {
 }
 
 mainMenuSelection();
+// queries.addRole();
